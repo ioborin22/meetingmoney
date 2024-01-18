74750c38854b1a0205cbfaa183cedf16385c73f0
ioborin22 <ioborin22@gmail.com>
2024-01-18 13:43:36 +0700

    20240118

---
+++ app/Http/Controllers/Api/OfferListingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\OfferListing;
+use Illuminate\Http\Request;
+
+class OfferListingController extends Controller
+{
+    public function index()
+    {
+        return OfferListing::with(['user' => function ($query) {
+            $query->withCount(['ratings as positive' => function ($query) {
+                $query->where('rating', '+');
+            }, 'ratings as negative' => function ($query) {
+                $query->where('rating', '-');
+            }]);
+        }])->get();
+    }
+}
